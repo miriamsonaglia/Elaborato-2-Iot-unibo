1b31e25f66ce0640ef02b40766a34e325c5ce973
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-11-27 16:29:33 +0100

    Added button task to scheduler.

--- src/arduino/src/smart_waste_disposal.cpp
+++ src/arduino/src/smart_waste_disposal.cpp
@@ -1,38 +1,46 @@
 #include "../lib/Tasks/LedTask.h"
 #include "../lib/Tasks/DoorTask.h"
 #include "../lib/Scheduling/Scheduler.h"
+#include "../lib/Tasks/ButtonsTask.h"
 #include <Arduino.h>
 
 #define RED_LED_PIN 12
 #define GREEN_LED_PIN 13
 #define DOOR_PIN 6
-#define BASE_PERIOD 1000
+#define OPEN_BUTTON 2
+#define CLOSE_BUTTON 4
+#define BASE_PERIOD 100
 
 /*Scheduler + task definition*/
 LedTask* task_for_leds;
 Scheduler* sched;
 DoorTask* task_for_door;
+ButtonsTask* task_for_button;
 
 /*Global variables*/
 int wError = 0;         //flag variable,checks if waste bin is full
 int tError = 0;         //flag variable,checks if there is a temperature problem
 int openDoor  = 0;      //flag command, if set to 1 indicates that the door should become open
 int closeDoor = 0;      //flag command, if set to 1 indicates that the door should become closed
-int emptyDoor = 1;      //flag command, if set to 1 indicates that the door should be in a -90 degree position to empty the waste bin,this flag should always take priority over open and close flags.
+int emptyDoor = 0;      //flag command, if set to 1 indicates that the door should be in a -90 degree position to empty the waste bin,this flag should always take priority over open and close flags.
 int sleep_mode = 0;     //flag variable,if set to 1 indicates that the system is in sleep mode,the LCD should be turned off and the leds freezed.
 
 void setup(){
     sched = new Scheduler();
     sched->init(BASE_PERIOD);
     /*Setup tasks*/
+    task_for_button = new ButtonsTask(OPEN_BUTTON,CLOSE_BUTTON);
+    task_for_button->init(BASE_PERIOD);
+    sched->addTask(task_for_button);
+
     task_for_leds = new LedTask(GREEN_LED_PIN,RED_LED_PIN);
-    task_for_leds->init(BASE_PERIOD);
+    task_for_leds->init(BASE_PERIOD*3);
     sched->addTask(task_for_leds);
 
     task_for_door = new DoorTask(DOOR_PIN);
     task_for_door->init(BASE_PERIOD*2);
     sched->addTask(task_for_door);
-    Serial.begin(9600);
+    
 }
 
 void loop(){
