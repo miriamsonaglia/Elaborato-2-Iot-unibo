9554e7cabd17a5bf3393a0bc34973dc27d250fdf
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-11-28 18:28:21 +0100

    LCD massege

--- src/arduino/src/LcdTask.cpp
+++ src/arduino/src/LcdTask.cpp
@@ -1,8 +1,9 @@
 #include "../Tasks/LcdTask.h"
 
+
 LcdTask::LcdTask(int address, int cols, int rows){
     this->lcd = new Lcd(address, cols, rows);
-    this->currentMsg = "Ciao";
+    this->currentMsg = "Press open to enter waste";
 }
 
 void LcdTask::init(int period){
@@ -15,5 +16,26 @@ void LcdTask::updateMsg(const char* msg){
 }
 
 void LcdTask::tick(){
-    lcd->message(currentMsg);
+    if(sleep_mode){
+        lcd->close();
+    }else if(tError){
+        lcd->message("PROBLEM DETECTED");
+
+    }else if(wError){
+        lcd->message("CONTAINER IS FULL");
+
+    }else if(openDoor){
+        lcd->message("PRESS CLOSE WHEN DONE");
+
+    }else if(closeDoor){
+
+
+    }else if(emptyDoor){
+        
+
+    }else
+    {
+        lcd->message("PRESS OPEN TO ENTER WASTE");
+    }
+    
 }
