fcb54b5fb6b93c3a95d23fd84e10943b9a5c3f9f
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2024-12-03 22:09:24 +0100

    removed debug lines

--- src/pc/src/OperatorDashboard.java
+++ src/pc/src/OperatorDashboard.java
@@ -101,7 +101,6 @@ public class OperatorDashboard {
                 String input ="";
                 if(portInput.hasNextLine()){
                     input = portInput.nextLine();
-                    System.out.println(input);
                 }
                 if (true) {    // Se il numero di bytes letti > 0, li converte in una Stringa
                     String data = input;
